8314537c06912b30e9c67851490e2072028920b5
ne2da <mailboxxx8@gmail.com>
2022-02-20 11:08:14 +0200

    Initial commit

---
+++ 01_intro.js
@@ -0,0 +1,16 @@
+var a;
+
+var b = 156;
+a = -15;
+
+console.log(a + b);
+
+const name = "James";
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
